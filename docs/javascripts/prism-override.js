d367c5485ad64dc5b18d799908a454e2d8440a17
RichardPreziosi <17318983+RichardPreziosi@users.noreply.github.com>
2025-12-26 01:08:51 -0500

    reverting some changes.

--- docs/javascripts/prism-override.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Let Prism re-highlight all code blocks after Material loads them
-    if (typeof Prism !== 'undefined') {
-        Prism.highlightAll();
-    }
-});
